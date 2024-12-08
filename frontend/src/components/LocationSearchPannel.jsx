6196de666dc19091fea1fe785f63620b6d1813a4
paammcakee@gmail.com <paammcakee@gmail.com>
2024-12-08 18:22:03 +0530

    Search-Vehicle-Done

--- frontend/src/components/LocationSearchPannel.jsx
+++ frontend/src/components/LocationSearchPannel.jsx
@@ -4,21 +4,21 @@ const LocationSearchPannel = () => {
   return (
     <div>
       {/* sample data */}
-      <div className='flex items-center justify-start gap-4 my-4'>
+      <div className='flex items-center justify-start gap-4 p-3 my-2 border-2 border-gray-100 rounded-xl active:border-black '>
         <h2 className='bg-[#eee] h-8 w-8 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill "></i></h2>
-        <h4 className='font-medium'>Umbhel, Moti-Sheri, Kmarej-Surat</h4>
+        <h4 className='font-medium'>Umbhel, Moti-Sheri, Kamrej-Surat</h4>
       </div>
-      <div className='flex items-center justify-start gap-4 my-4'>
+      <div className='flex items-center justify-start gap-4 p-3 my-2 border-2 border-gray-100 rounded-xl active:border-black '>
         <h2 className='bg-[#eee] h-8 w-8 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill "></i></h2>
-        <h4 className='font-medium'>Umbhel, Moti-Sheri, Kmarej-Surat</h4>
+        <h4 className='font-medium'>Umbhel, Moti-Sheri, Kamrej-Surat</h4>
       </div>
-      <div className='flex items-center justify-start gap-4 my-4'>
+      <div className='flex items-center justify-start gap-4 p-3 my-2 border-2 border-gray-100 rounded-xl active:border-black '>
         <h2 className='bg-[#eee] h-8 w-8 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill "></i></h2>
-        <h4 className='font-medium'>Umbhel, Moti-Sheri, Kmarej-Surat</h4>
+        <h4 className='font-medium'>Umbhel, Moti-Sheri, Kamrej-Surat</h4>
       </div>
-      <div className='flex items-center justify-start gap-4 my-4'>
+      <div className='flex items-center justify-start gap-4 p-3 my-2 border-2 border-gray-100 rounded-xl active:border-black '>
         <h2 className='bg-[#eee] h-8 w-8 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill "></i></h2>
-        <h4 className='font-medium'>Umbhel, Moti-Sheri, Kmarej-Surat</h4>
+        <h4 className='font-medium'>Umbhel, Moti-Sheri, Kamrej-Surat</h4>
       </div>
       
     </div>
