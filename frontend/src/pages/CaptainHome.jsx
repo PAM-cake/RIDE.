6bcb330fd954c7b020df04444431c79f633d265c
paammcakee@gmail.com <paammcakee@gmail.com>
2025-01-03 03:36:21 -0600

    User UI & Captain-home Done!

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,10 +1,62 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 // Captain home component
 const CaptainHome = () => {
   return (
-    <div>CaptainHome</div>
-  )
-}
+    <div className="h-screen">
+      <div className="fixed flex items-center justify-between w-screen p-6">
+        <img
+          className="w-16 left-5 top-5"
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt=""
+        />
+        <Link
+          to="/home"
+          className="flex items-center justify-center w-10 h-10 bg-white rounded-full "
+        >
+          <i className="text-lg font-bold ri-logout-box-r-line"></i>
+        </Link>
+      </div>
+
+      <div className="h-3/5">
+        <img
+          className="object-cover w-full h-full"
+          src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
+          alt=""
+        />
+      </div>
+      <div className="p-6 h-2/5">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center justify-start gap-2">
+            <img className="object-cover w-10 h-10 rounded-full" src="https://plus.unsplash.com/premium_photo-1689551670902-19b441a6afde?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww" alt="" />
+            <h4 className="text-lg font-medium">Veer Patel</h4>
+          </div>
+          <div>
+            <h4 className="text-xl font-semibold">₹300.50</h4>
+            <p className="text-sm text-gray-800">Earned</p>
+          </div>
+        </div>
+        <div className="flex justify-center gap-5 p-3 mt-8 bg-gray-100 rounded-2xl">
+          <div className="text-center">
+            <i className="mb-2 text-3xl font-thin ri-time-line"></i>
+            <h5 className="text-lg font-medium">4.3</h5>
+            <p className="text-sm text-gray-600">Hours Online</p>
+          </div>
+          <div className="text-center">
+            <i className="mb-2 text-3xl font-thin ri-speed-up-line"></i>
+            <h5 className="text-lg font-medium">4.3</h5>
+            <p className="text-sm text-gray-600">Hours Online</p>
+          </div>
+          <div className="text-center">
+            <i className="mb-2 text-3xl font-thin ri-booklet-line"></i>
+            <h5 className="text-lg font-medium">4.3</h5>
+            <p className="text-sm text-gray-600">Hours Online</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CaptainHome;
--- frontend/src/pages/Riding.jsx
+++ frontend/src/pages/Riding.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";  
+import { Link } from "react-router-dom"; 
 
-const Riding = () => {
+const Riding = () => {        
   return (
-    <div>Riding</div>
-  )
-}
+    <div className="h-screen"> 
+      <Link to="/home" className="fixed flex items-center justify-center w-10 h-10 bg-white rounded-full top-5 right-5">
+        <i className="text-lg font-bold ri-home-9-fill"></i>
+      </Link>
+      
+      <div className="h-1/2">
+        <img
+          className="object-cover w-full h-full"
+          src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
+          alt=""
+        />
+      </div>
+      <div className="p-4 h-1/2">
+        <div className="flex items-center justify-between gap-2">
+          <img
+            className="h-12"
+            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_638,w_956/v1555367349/assets/d7/3d4b80-1a5f-4a8b-ac2b-bf6c0810f050/original/Final_XL.png"
+            alt=""
+          />
+          <div className="text-right">
+            <h2 className="text-lg font-medium">Param</h2>
+            <h4 className="-mt-1 -mb-1 text-xl font-semibold">GJ05 JR 3340</h4>
+            <p className="text-sm text-gray-500">Volkswagen Vento</p>
+          </div>
+        </div>
 
-export default Riding
+        <div className="flex flex-col items-center justify-between gap-2">
+          {/* Vehicle image */}
+          <div className="w-full mt-5">
+            {/* Destination location */}
+            <div className="flex items-center gap-5 p-3 border-b-2">
+              <i className="text-lg ri-map-pin-2-fill"></i>
+              <div>
+                <h3 className="text-lg font-medium">7709/1960</h3>
+                <p className="-mt-1 text-sm text-gray-600">Umbhel Surat</p>
+              </div>
+            </div>
+            {/* Fare details */}
+            <div className="flex items-center gap-5 p-3">
+              <i className="text-lg ri-currency-line"></i>
+              <div>
+                <h3 className="text-lg font-medium">₹150.00</h3>
+                <p className="-mt-1 text-sm text-gray-600">Cash</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button className="w-full p-2 mt-5 font-semibold text-white bg-green-600 rounded-lg">
+        <i className="ri-bank-card-line"></i> Make a Payment
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Riding;
